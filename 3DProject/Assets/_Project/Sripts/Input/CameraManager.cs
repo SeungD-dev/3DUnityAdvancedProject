bed1f489ce9c2d9fd48210ef3726a595e2605019
SeungD-dev <jyyootom@naver.com>
2024-05-29 12:55:20 +0900

    Feat : 각종 에셋 추가 및 플레이어 이동과 애니메이션, 카메라

---
+++ 3DProject/Assets/_Project/Sripts/Input/CameraManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using KBCore.Refs;
+using Cinemachine;
+using Unity.VisualScripting;
+using System;
+using System.Collections;
+
+namespace Platformer
+{
+    public class CameraManager : ValidatedMonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField, Anywhere] InputReader input;
+        [SerializeField, Anywhere] CinemachineFreeLook freeLookVCam;
+
+        [Header("Settings")]
+        [SerializeField, Range(0.5f, 3f)] float speedMuliplier = 1f;
+
+        bool isRMBPressed;
+        bool cameraMovementLock;
+
+        private void OnEnable()
+        {
+            input.Look += OnLook;
+            input.EnableMouseControlCamera += OnEnableMouseControlCamera;
+            input.DisableMouseControlCamera += OnDisableMouseControlCamera;
+        }
+
+        private void OnDisable()
+        {
+            input.Look -= OnLook;
+            input.EnableMouseControlCamera -= OnEnableMouseControlCamera;
+            input.DisableMouseControlCamera -= OnDisableMouseControlCamera;
+        }
+
+        private void OnLook(Vector2 cameraMovement, bool isDeviceMouse)
+        {
+            if (cameraMovementLock) return;
+
+            if (isDeviceMouse && !isRMBPressed) return;
+
+            //If the device is mouse use fixedDeltaTime, otherwise use deltaTime
+            float deviceMultiplier = isDeviceMouse ? Time.fixedDeltaTime : Time.deltaTime;
+
+            //Set the camera axis values
+            freeLookVCam.m_XAxis.m_InputAxisValue = cameraMovement.x * speedMuliplier * deviceMultiplier;
+            freeLookVCam.m_YAxis.m_InputAxisValue = cameraMovement.y * speedMuliplier * deviceMultiplier;
+        }
+
+        private void OnEnableMouseControlCamera()
+        {
+            isRMBPressed = true;
+
+            //Lock the cursor to the center of the screen and hide it
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            StartCoroutine(DisableMouseForFrame());
+        }
+
+        private void OnDisableMouseControlCamera()
+        {
+            isRMBPressed = false;
+
+            //UnLock the cursor and make it visible
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            //Reset the camera axis to prevent jumping when re-enabling mouse control
+            freeLookVCam.m_XAxis.m_InputAxisValue = 0f;
+            freeLookVCam.m_YAxis.m_InputAxisValue = 0f;
+        }
+
+        IEnumerator DisableMouseForFrame()
+        {
+            cameraMovementLock = true;
+            yield return new WaitForEndOfFrame();
+            cameraMovementLock = false;
+        }
+
+     
+    }
+}
